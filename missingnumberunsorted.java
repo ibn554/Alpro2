47afb7196c2ab967f11c77c593aef4f5e24b89d6
ibnu azam <ibnuazam4577@gmail.com>
2025-05-15 14:36:36 +0700

    pertemuan 9

---
+++ missingnumberunsorted.java
@@ -0,0 +1,29 @@
+public class missingnumberunsorted {
+    
+public class MissingNumberUnsorted {
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 6, 2, 7, 4}; 
+
+        int min = arr[0];
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) min = arr[i];
+            if (arr[i] > max) max = arr[i];
+        }
+
+        int[] b = new int[max + 1]; 
+
+        for (int i = 0; i < arr.length; i++) {
+
+            b[arr[i]] = 1;
+        }
+
+        for (int i = min; i <= max; i++) {
+            if (b[i] == 0) {
+                System.out.println("Bilangan yang hilang: " + i);
+            }
+        }
+    }
+}
+
+}
